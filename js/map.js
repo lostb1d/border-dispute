50e108122bd8943c03dc60a6c9490883c6b470c8
lostb1d <mylife.dinesh08@gmail.com>
2020-05-20 11:42:02 +0545

    Map Added

---
+++ js/map.js
@@ -0,0 +1,9 @@
+var mymap = L.map('mapid').setView([33.39,90.44], 4);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibG9zdGIxIiwiYSI6ImNrYWV3eDBhYTA3aGYycm9nMDMzN3g3cGgifQ.PFbvBYtrGPHUW2UhJjA7xg'
+}).addTo(mymap);
